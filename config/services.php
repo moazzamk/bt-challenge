5ead5c26a01db652a0ab71fda0ea958e458ed3d6
Khan <moazzam.khan@balfour.com>
2016-12-21 14:09:59 -0600

    services

--- config/services.php
+++ config/services.php
@@ -1,7 +1,23 @@
 <?php
 
 return [
-    'CreditCardValidator' => function () {
+    'Validator\CreditCardValidator' => function () {
         return new \Challenge\Validator\CreditCardValidator();
-    }
+    },
+    'Customer\AddAction' => function ($container) {
+        return new \Challenge\Action\Customer\AddAction(
+            $container->get('Validator\CreditCardValidator'),
+            $container->get('EntityManager')
+        );
+    },
+    'Customer\ChargeAction' => function ($container) {
+        return new \Challenge\Action\Customer\ChargeAction($container->get('EntityManager'));
+    },
+    'Customer\CreditAction' => function ($container) {
+        return new \Challenge\Action\Customer\CreditAction($container->get('EntityManager'));
+    },
+    'Customer\SummaryAction' => function ($container) {
+        return new \Challenge\Action\Customer\SummaryAction($container->get('EntityManager')->getRepository('\Challenge\Entity\Customer'));
+    },
+
 ];
